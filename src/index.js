49da42121910bebcdd29ef07422eb72e73c487c9
abuck199 <alex.footbal@gmail.com>
2025-11-03 17:16:33 -0500

    add vercel tracking analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import SecretSantaApp from './SecretSanta';
 
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHA_SITE_KEY || ''}>
       <SecretSantaApp />
+      <Analytics />
     </GoogleReCaptchaProvider>
   </React.StrictMode>
 );
